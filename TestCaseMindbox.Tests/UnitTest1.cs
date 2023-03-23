e5bdb73d15c7aea575d01dfd17830e5d37c54946
roman <rdandrievskiy@edu.hse.ru>
2023-03-23 17:09:58 +0300

    upd

--- TestCaseMindbox.Tests/UnitTest1.cs
+++ TestCaseMindbox.Tests/UnitTest1.cs
@@ -15,6 +15,28 @@ namespace TestCaseMindbox.Tests
 		public void TestTriangle()
 		{
 			Assert.AreEqual(SquareCalculations.Triangle(3, 4, 5), 6);
+			Assert.IsFalse(SquareCalculations.TriangleExists(1, 1, 100));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => SquareCalculations.Triangle(-1, 1, 1));
+		}
+	}
+
+	[TestClass]
+	public class TestObjects
+	{
+		[TestMethod]
+		public void TestTriangle()
+		{
+			Assert.AreEqual(new Triangle(3, 4, 5).Square, SquareCalculations.Triangle(3, 4, 5));
+			Assert.IsTrue(new Triangle(0.3, 0.4, 0.5).IsRight);
+			Assert.IsFalse(new Triangle(1, 0.5, 0.5).IsRight);
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Triangle(1, 0.44444, 0.5));
+		}
+
+		[TestMethod]
+		public void TestCircle()
+		{
+			Assert.AreEqual(new Circle(5).Square, SquareCalculations.Circle(5));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new Circle(-1));
 		}
 	}
 }
--- TestCaseMindbox/SquareCalculations.cs
+++ TestCaseMindbox/SquareCalculations.cs
@@ -26,14 +26,73 @@
 		/// <param name="c"></param>
 		/// <returns>square of triangle with given sides lengths</returns>
 		/// <exception cref="ArgumentOutOfRangeException">Raised when one or more of given lengths are negative</exception>
-		public static double Triangle(double a, double b, double c) 
+		public static double Triangle(double a, double b, double c)
 		{
-			if (a < 0 || b < 0 || c < 0)
+			if (!TriangleExists(a, b, c))
 			{
 				throw new ArgumentOutOfRangeException("");
 			}
 			var p = (a + b + c) * 0.5;
 			return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+		}
+
+		/// <summary>
+		/// Checks if triangle exists.
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name="c"></param>
+		/// <returns></returns>
+		public static bool TriangleExists(double a, double b, double c)
+		{
+			if (a < 0 || b < 0 || c < 0)
+			{
+				return false;
+			}
+			var sorted = new[] { a, b, c }.OrderBy(x => x).ToArray();
+			return sorted[0] + sorted[1] >= sorted[2];
+		}
+	}
+
+	/// <summary>
+	/// Defines an interface of a solid square of which can be evaluated
+	/// </summary>
+	public interface ISquareEvaluable
+	{
+		double Square { get; }
+	}
+
+	/// <summary>
+	/// Defines a triangle type.
+	/// Provides square evaluation and checks if Triangle is right
+	/// </summary>
+	public class Triangle : ISquareEvaluable
+	{
+		public double Square { get; private set; }
+		public bool IsRight { get; private set; }
+		public Triangle(double a, double b, double c)
+		{
+			Square = SquareCalculations.Triangle(a, b, c);
+
+			// Checking if triangle is right
+			var sides = new[] { a, b, c }.OrderBy(x => x).ToArray();
+			if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2])
+			{
+				IsRight = true;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Defines a circle type, provides square evaluation.
+	/// </summary>
+	public class Circle : ISquareEvaluable
+	{
+		public double Square { get; private set; }
+
+		public Circle(double radius)
+		{
+			Square = SquareCalculations.Circle(radius);
 		}
 	}
 }
